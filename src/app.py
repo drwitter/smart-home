809a60faba1db5bf77e25f8b3fb9db069b86e44b
Davey Witter <davey.r.witter@gmail.com>
2023-11-20 20:05:16 +0100

    Argparse to hub

--- src/app.py
+++ src/app.py
@@ -1,4 +1,34 @@
 import argparse
+import os
 
+from dotenv import load_dotenv
+
+from src.hub.hub import hub
+
+'''
+    Always make sure to set the environment variable room
+    --------------------------------------------
+    Example:
+    - python src/app.py hub
+    - python src/app.py sensor --sensor temp
+'''
 if __name__ == "__main__":
-    pass
+    load_dotenv()
+    room = os.environ.get('room')
+
+    parser = argparse.ArgumentParser(
+                    prog='smart-home',
+                    description='Smart home application')
+    parser.add_argument('app', choices=['hub', 'sensor'], help='Type of the device')
+    parser.add_argument('-t', '--type', required=False, help='Type of the sensor', choices=['temp','motion'])
+    
+    args = parser.parse_args()
+    
+    match args.app:
+        case 'hub':
+            rpi_hub = hub()
+            rpi_hub.start()
+        case 'sensor':
+            print('Starting sensor')
+        case _:
+            print('Unknown device type')
